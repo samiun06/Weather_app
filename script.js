4c8a8b4ee8debc37dccfc6d14c4177a378e95a4e
Samiun Jahan <samiunjahan06@gmail.com>
2021-02-04 18:24:13 +0600

    some changes

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ const apiKey = "8517f8a5d8e1b0cce83c0f5bfed42c1b";
 const apiBase = "https://api.openweathermap.org/data/2.5/weather";
 
 const getWeather = city => {
-    const url = `${apiBase}?q=${city}&units=metric&appid=${apiKey}`
+    const url = `${apiBase}?q=${city}&units=metric&appid=${apiKey}`;
     fetch(url)
-        .then(res => res.json())
-        .then(data => updateWeather(data));
+        .then(response => response.json())
+        .then(data => updateWeather(data))
 }
 
-document.getElementById("search").addEventListener("click", () => {
-    const cityInput = document.getElementById("city-input").value;
-    getWeather(cityInput);
+document.querySelector("#search-btn").addEventListener("click", () => {
+    const getWeatherInput = document.querySelector("#searched-city").value;
+    getWeather(getWeatherInput);
 })
 
-function updateWeather(data) {
-    document.getElementById("city-name").innerText = data.name || "unknown city";
-    document.getElementById("temp").innerText = data.main.temp;
-    document.getElementById("weather").innerText = data.weather[0].main;
-    document.getElementById("icon").setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
-    document.getElementById("city-input").value = "";
+const updateWeather = data => {
+    document.querySelector("#city-name").innerText = data.name;
+    document.querySelector("#temperature").innerText = data.main.temp;
+    document.querySelector(".lead").innerText = data.weather[0].main;
+    document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+    document.querySelector("#searched-city").value = "";
 }
 
 getWeather("Dhaka");
